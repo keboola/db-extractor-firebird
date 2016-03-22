d21418af2392199ec1429be970ac89256204941c
Miroslav Cillik <miro@keboola.com>
2016-03-22 12:53:07 +0100

    feat: config definition

---
+++ src/Keboola/DbExtractor/Configuration/FirebirdConfigDefinition.php
@@ -0,0 +1,90 @@
+<?php
+use Keboola\DbExtractor\Configuration\ConfigDefinition;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+
+/**
+ * Created by PhpStorm.
+ * User: miroslavcillik
+ * Date: 22/03/16
+ * Time: 12:47
+ */
+class FirebirdConfigDefinition extends ConfigDefinition
+{
+    /** @inherit */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('config');
+
+        $rootNode
+            ->children()
+            ->scalarNode('data_dir')
+            ->isRequired()
+            ->cannotBeEmpty()
+            ->end()
+            ->scalarNode('extractor_class')
+            ->isRequired()
+            ->cannotBeEmpty()
+            ->end()
+            ->arrayNode('parameters')
+            ->children()
+                ->arrayNode('db')
+                    ->children()
+                        ->scalarNode('dbname')
+                            ->isRequired()
+                            ->cannotBeEmpty()
+                        ->end()
+                        ->scalarNode('user')
+                            ->isRequired()
+                        ->end()
+                        ->scalarNode('password')->end()
+                        ->scalarNode('#password')->end()
+                        ->append($this->addSshNode())
+                    ->end()
+                ->end()
+            ->arrayNode('tables')
+            ->isRequired()
+            ->prototype('array')
+            ->children()
+            ->integerNode('id')
+            ->isRequired()
+            ->min(0)
+            ->end()
+            ->scalarNode('name')
+            ->isRequired()
+            ->cannotBeEmpty()
+            ->end()
+            ->scalarNode('query')
+            ->isRequired()
+            ->cannotBeEmpty()
+            ->end()
+            ->scalarNode('outputTable')
+            ->isRequired()
+            ->cannotBeEmpty()
+            ->end()
+            ->booleanNode('incremental')
+            ->defaultValue(false)
+            ->end()
+            ->booleanNode('enabled')
+            ->defaultValue(true)
+            ->end()
+            ->scalarNode('primaryKey')
+            ->defaultValue(null)
+            ->end()
+            ->integerNode('retries')
+            ->min(1)
+            ->end()
+            ->end()
+            ->end()
+            ->end()
+            ->end()
+            ->end()
+            ->arrayNode('image_parameters')
+            ->end()
+            ->end()
+        ;
+
+        return $treeBuilder;
+    }
+
+}
