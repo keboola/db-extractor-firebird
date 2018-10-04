5891eb53f42dd996085f39374117163c8e90686e
pivnicek <marc@keboola.com>
2018-10-04 12:13:42 +0200

    switch tests to different suites to avoid ssh collision

--- tests/Keboola/DbExtractor/RunTest.php
+++ tests/Keboola/DbExtractor/RunTest.php
@@ -3,12 +3,37 @@
 
 namespace Keboola\DbExtractor;
 
+use Keboola\DbExtractor\Test\ExtractorTest;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Yaml\Yaml;
 
-class RunTest extends FirebirdTest
+class RunTest extends ExtractorTest
 {
     public const ROOT_PATH = __DIR__ . '/../../..';
+
+    protected function getConfig($driver)
+    {
+        $config = Yaml::parse(file_get_contents($this->dataDir . '/' . $driver . '/config.yml'));
+        $config['parameters']['data_dir'] = $this->dataDir;
+        $config['parameters']['extractor_class'] = 'Firebird';
+
+        if (false === getenv(strtoupper($driver) . '_DB_USER')) {
+            throw new \Exception("DB_USER envrionment variable must be set.");
+        }
+
+        if (false === getenv(strtoupper($driver) . '_DB_PASSWORD')) {
+            throw new \Exception("DB_PASSWORD envrionment variable must be set.");
+        }
+
+        $config['parameters']['db']['user'] = getenv(strtoupper($driver) . '_DB_USER');
+        $config['parameters']['db']['password'] = getenv(strtoupper($driver) . '_DB_PASSWORD');
+
+        if (false !== getenv(strtoupper($driver) . '_DB_DBNAME')) {
+            $config['parameters']['db']['dbname'] = getenv(strtoupper($driver) . '_DB_DBNAME');
+        }
+
+        return $config;
+    }
 
     public function testTestConnectionAction(): void
     {
@@ -52,5 +77,11 @@ class RunTest extends FirebirdTest
         $this->assertEquals(0, $process->getExitCode());
         $this->assertEquals("", $process->getErrorOutput());
         $this->assertJson($process->getOutput());
+    }
+
+    public function getFirebirdPrivateKey(): string
+    {
+        // docker-compose .env file does not support new lines in variables so we have to modify the key https://github.com/moby/moby/issues/12997
+        return str_replace('"', '', str_replace('\n', "\n", $this->getEnv('firebird', 'DB_SSH_KEY_PRIVATE')));
     }
 }
