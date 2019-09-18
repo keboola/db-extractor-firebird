544064bd7ce58ad6e0bca94a954f3b6f63fef2b6
Ondra Jodas <ondra.jodas@gmail.com>
2019-09-18 13:36:31 +0200

    fixup! validation

--- src/Keboola/DbExtractor/Configuration/NodeDefinition/FirebirdTablesNode.php
+++ src/Keboola/DbExtractor/Configuration/NodeDefinition/FirebirdTablesNode.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Keboola\DbExtractor\Configuration\NodeDefinition;
 
 use Keboola\DbExtractorConfig\Configuration\NodeDefinition\TablesNode;
-use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
 
 class FirebirdTablesNode extends TablesNode
 {
@@ -14,19 +13,6 @@ class FirebirdTablesNode extends TablesNode
         // @formatter:off
         $this
             ->prototype('array')
-            ->validate()->always(function ($v) {
-                if (isset($v['query']) && $v['query'] !== '' && isset($v['table'])) {
-                    throw new InvalidConfigurationException('Both table and query cannot be set together.');
-                }
-                if (isset($v['query']) && $v['query'] !== '' && isset($v['incrementalFetchingColumn'])) {
-                    $message = 'Incremental fetching is not supported for advanced queries.';
-                    throw new InvalidConfigurationException($message);
-                }
-                if (!isset($v['table']) && !isset($v['query'])) {
-                    throw new InvalidConfigurationException('One of table or query is required');
-                }
-                return $v;
-            })->end()
             ->children()
                 ->integerNode('id')
                     ->isRequired()
@@ -36,7 +22,9 @@ class FirebirdTablesNode extends TablesNode
                     ->isRequired()
                     ->cannotBeEmpty()
                 ->end()
-                ->scalarNode('query')->end()
+                ->scalarNode('query')
+                    ->isRequired()
+                ->end()
                 ->scalarNode('outputTable')
                     ->isRequired()
                     ->cannotBeEmpty()
