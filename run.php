3e386b40aee36d169ab561dd27f3f9cc299e068d
Ondra Jodas <ondrej.jodas@keboola.com>
2019-12-27 11:30:33 +0100

    read output state

--- run.php
+++ run.php
@@ -8,6 +8,7 @@ use Keboola\DbExtractorLogger\Logger;
 use Monolog\Handler\NullHandler;
 use Symfony\Component\Serializer\Encoder\JsonEncode;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\JsonDecode;
 
 require_once(__DIR__ . "/vendor/autoload.php");
 
@@ -25,7 +26,18 @@ try {
         throw new UserException("Could not find a valid configuration file.");
     }
 
-    $app = new FirebirdApplication($config, $arguments['data'], $logger);
+    // get the state
+    $inputState = [];
+    $inputStateFile = $arguments["data"] . '/in/state.json';
+    if (file_exists($inputStateFile)) {
+        $jsonDecode = new JsonDecode(true);
+        $inputState = $jsonDecode->decode(
+            (string) file_get_contents($inputStateFile),
+            JsonEncoder::FORMAT
+        );
+    }
+
+    $app = new FirebirdApplication($config, $arguments['data'], $logger, $inputState);
 
     $runAction = true;
     if ($app['action'] !== 'run') {
