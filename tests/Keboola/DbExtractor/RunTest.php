74b0b3031e754b7f51555d447f5c941360d03195
Ondra Jodas <ondrej.jodas@keboola.com>
2020-04-17 09:20:40 +0200

    test for connection via ssh proxy

--- tests/Keboola/DbExtractor/RunTest.php
+++ tests/Keboola/DbExtractor/RunTest.php
@@ -46,6 +46,33 @@ class RunTest extends FirebirdBaseTest
         $this->assertEquals('', $process->getErrorOutput());
     }
 
+    public function testRunActionSshTunnel(): void
+    {
+        $config = $this->getConfig(self::DRIVER);
+        $config['parameters']['db']['ssh'] = [
+            'enabled' => true,
+            'keys' => [
+                '#private' => $this->getPrivateKey(),
+                'public' => $this->getPublicKey(),
+            ],
+            'user' => 'root',
+            'sshHost' => 'sshproxy',
+            'remoteHost' => 'firebird',
+            'remotePort' => '3050',
+            'localPort' => '33339',
+        ];
+
+        $this->replaceConfig($config);
+
+        $process = new Process(['php', self::ROOT_PATH . '/run.php', '--data=' . $this->dataDir]);
+        $process->setTimeout(300);
+        $process->run();
+
+        $this->assertEquals(0, $process->getExitCode());
+        $this->assertStringContainsString('Creating SSH tunnel to \'sshproxy\'', $process->getOutput());
+        $this->assertStringContainsString('host=127.0.0.1;port=33339', $process->getOutput());
+    }
+
     public function testSshTestConnection(): void
     {
         $config = $this->getConfig(self::DRIVER);
